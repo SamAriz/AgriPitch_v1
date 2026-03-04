f380fee373520dfd27ac5783f379f0e86a5b640e
PzADaLor <bingil.sa@southernchristiancollege.edu.ph>
2026-03-04 20:53:25 +0800

    Delete farmers_fixed directory

--- farmers_fixed/src/app/components/marketplace/MyListings.tsx
+++
@@ -1,6 +0,0 @@
-import { FarmerProducts } from '../farmer/FarmerProducts';
-
-// Reuse the FarmerProducts component for marketplace listings
-export function MyListings() {
-  return <FarmerProducts />;
-}
